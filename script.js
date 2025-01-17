f868603e6099a250c60cdad75c7d2ac599563fbd
Atakan Arpacı <130912588+oppatates@users.noreply.github.com>
2025-01-17 14:09:32 +0300

    Update script.js (non value input)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function randomNumber() {
-    const min = parseInt(document.getElementById('min').value, 10);
-    const max = parseInt(document.getElementById('max').value, 10);
+    const min = parseInt(document.getElementById('min').value,);
+    const max = parseInt(document.getElementById('max').value,);
     const res = document.getElementById('result');
 
     const sonuc = Math.floor(Math.random() * (max - min + 1)) + min;
